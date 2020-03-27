b415213b5ef5397c106c236d3678921210dbdf4e
Romel <romel@mobonapps.com>
2020-03-27 14:51:00 +0800

    Code cleanup

--- js/covid-19.js
+++ js/covid-19.js
@@ -1,12 +1,13 @@
 const Sharer = {
   init: () => {
-    const twitterParams = $.param({ text: `Great article from ${window.location.href}` });
+    const url = window.location.href;
+
+    const twitterParams = $.param({ url, text: `Great article from ${url}` });
     const twitterHref = `https://twitter.com/intent/tweet?${twitterParams}`;
     $('a.twitter-share-button').attr('href', twitterHref);
 
     const title = $('.h1.title').text();
-    const inParams = $.param({ url: window.location.href, mini: true, source: 'BPLDigital', title });
-    console.log(inParams);
+    const inParams = $.param({ url, title, mini: true, source: 'BPLDigital' });
     const inHref = `https://www.linkedin.com/shareArticle?${inParams}`;
     $('a.in-share-button').attr('href', inHref);
   }
